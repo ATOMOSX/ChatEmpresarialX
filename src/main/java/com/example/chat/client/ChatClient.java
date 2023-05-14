8f8c00e3cde33ae045b7b514c1bd9ee3ccf70ccc
XATOMOS <juand.lopezm@uqvirtual.edu.co>
2023-05-14 12:24:57 -0500

    Se modifica la clase ChatClient

--- src/main/java/com/example/chat/client/ChatClient.java
+++ src/main/java/com/example/chat/client/ChatClient.java
@@ -1,110 +1,64 @@
 package com.example.chat.client;
 
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import com.example.chat.ChatMessage;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ChatClient extends Application {
-
+public class ChatClient {
+    private String serverAddress;
+    private int serverPort;
     private Socket socket;
-    private TextArea chatArea;
-    private TextField messageField;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
+    private MessageListener messageListener;
 
-    public static void main(String[] args) {
-        launch(args);
+    public ChatClient(String serverAddress, int serverPort, MessageListener messageListener) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+        this.messageListener = messageListener;
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public void connect() throws IOException {
+        socket = new Socket(serverAddress, serverPort);
+        System.out.println("Connected to server " + serverAddress + " on port " + serverPort);
 
-        Label nameLabel = new Label("Username:");
-        TextField nameField = new TextField();
-        Button connectButton = new Button("Connect");
+        outputStream = new ObjectOutputStream(socket.getOutputStream());
+        inputStream = new ObjectInputStream(socket.getInputStream());
 
-        GridPane loginPane = new GridPane();
-        loginPane.setAlignment(Pos.CENTER);
-        loginPane.setHgap(10);
-        loginPane.setVgap(10);
-        loginPane.setPadding(new Insets(25, 25, 25, 25));
-        loginPane.add(nameLabel, 0, 0);
-        loginPane.add(nameField, 1, 0);
-        loginPane.add(connectButton, 2, 0);
-
-        VBox chatPane = new VBox();
-        chatPane.setSpacing(10);
-        chatPane.setPadding(new Insets(25, 25, 25, 25));
-        chatArea = new TextArea();
-        chatArea.setEditable(false);
-        chatArea.setWrapText(true);
-        ScrollPane scrollPane = new ScrollPane(chatArea);
-        scrollPane.setFitToWidth(true);
-        scrollPane.setFitToHeight(true);
-        messageField = new TextField();
-        Button sendButton = new Button("Send");
-        sendButton.setOnAction(event -> sendMessage());
-        chatPane.getChildren().addAll(scrollPane, messageField, sendButton);
-
-        BorderPane root = new BorderPane();
-        root.setCenter(chatPane);
-
-        Scene scene = new Scene(root, 600, 400);
-
-        primaryStage.setTitle("Chat Client");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        connectButton.setOnAction(event -> {
-            String username = nameField.getText().trim();
-            if (!username.isEmpty()) {
+        Thread receivingThread = new Thread(() -> {
+            while (true) {
                 try {
-                    socket = new Socket("localhost", 8080);
-                    chatArea.appendText("Connected to server!\n");
-                    ClientThread clientThread = new ClientThread(socket, this::onMessageReceived);
-                    clientThread.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    ChatMessage message = (ChatMessage) inputStream.readObject();
+                    messageListener.onMessageReceived(message);
+                } catch (IOException | ClassNotFoundException e) {
+                    System.err.println("Error receiving message: " + e.getMessage());
                 }
             }
         });
 
-        primaryStage.setOnCloseRequest(event -> {
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            Platform.exit();
-        });
-
+        receivingThread.start();
     }
 
-    private void onMessageReceived(String message) {
-        chatArea.appendText(message + "\n");
-    }
-
-    private void sendMessage() {
-        String message = messageField.getText().trim();
-        if (!message.isEmpty() && socket != null) {
-            ClientThread.sendMessage(message);
-            messageField.clear();
+    public void disconnect() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("Error disconnecting: " + e.getMessage());
         }
     }
 
-}
+    public void sendMessage(ChatMessage message) {
+        try {
+            outputStream.writeObject(message);
+        } catch (IOException e) {
+            System.err.println("Error sending message: " + e.getMessage());
+        }
+    }
 
+    public interface MessageListener {
+        void onMessageReceived(ChatMessage message);
+    }
+}
